5485d0c0b6a5fe8d2cd0f638611b367740304b00
kotik555555555 <kotikmurcik11@gmail.com>
2025-03-22 17:17:11 +0200

    save changes

--- handlers/inline_handlers.py
+++ handlers/inline_handlers.py
@@ -1,5 +1,5 @@
 from aiogram import types, Router
-from keyboards.reply_keyboards  import get_main_keyboard
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 router = Router()
 
@@ -13,6 +13,18 @@ router = Router()
 #         await callback_query.message.delete()
 #         await callback_query.message.answer("You pressed second inline button", reply_markup=get_inline_test2())
 
+async def show_main_menu(message):
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Готові пропозиції"), KeyboardButton(text="Пропозиції по містам")],
+            [KeyboardButton(text="Бронювання"), KeyboardButton(text="Акції")],
+            [KeyboardButton(text="Контакти"), KeyboardButton(text="Пасхалочка")]
+        ],
+        resize_keyboard=True
+    )
+    await message.answer("Головне меню:", reply_markup=keyboard)
+
+
 @router.callback_query(lambda c:  c.data.startswith("btn_1."))
 async def city_callback_handler(callback_query: types.CallbackQuery):
     data = callback_query.data
@@ -21,7 +33,8 @@ async def city_callback_handler(callback_query: types.CallbackQuery):
     elif data == "btn_1.2":
         await callback_query.message.answer("Ціна(за спаданням):")
     elif data == "btn_1.3":
-        await callback_query.message.answer("В процесі...")
+        await callback_query.message.answer("Повернення в головне меню...")
+        await show_main_menu(callback_query.message)
 
 @router.callback_query(lambda c:  c.data.startswith("btn_2."))
 async def city_callback_handler(callback_query: types.CallbackQuery):
@@ -33,7 +46,8 @@ async def city_callback_handler(callback_query: types.CallbackQuery):
     elif data == "btn_2.3":
         await callback_query.message.answer("Введіть назву міста:")
     elif data == "btn_2.4":
-        await callback_query.message.answer("В процесі...")
+        await callback_query.message.answer("Повернення в головне меню...")
+        await show_main_menu(callback_query.message)
 
 @router.callback_query(lambda c:  c.data.startswith("btn_3."))
 async def city_callback_handler(callback_query: types.CallbackQuery):
@@ -41,7 +55,8 @@ async def city_callback_handler(callback_query: types.CallbackQuery):
     if data == "btn_3.1":
         await callback_query.message.answer("Опишіть ваше замовлення:")
     elif data == "btn_3.2":
-        await callback_query.message.answer("В процесі...")
+        await callback_query.message.answer("Повернення в головне меню...")
+        await show_main_menu(callback_query.message)
 
 @router.callback_query(lambda c:  c.data.startswith("btn_4."))
 async def city_callback_handler(callback_query: types.CallbackQuery):
@@ -49,12 +64,15 @@ async def city_callback_handler(callback_query: types.CallbackQuery):
     if data == "btn_4.1":
         await callback_query.message.answer("Список акцій:")
     elif data == "btn_4.2":
-        await callback_query.message.answer("В процесі...")
+        await callback_query.message.answer("Повернення в головне меню...")
+        await show_main_menu(callback_query.message)
 
 @router.callback_query(lambda c:  c.data.startswith("btn_5."))
 async def city_callback_handler(callback_query: types.CallbackQuery):
     data = callback_query.data
     if data == "btn_5.1":
-        await callback_query.message.answer("Потім напишу.")
+        await callback_query.message.answer("ВИКОНАВ РОБОТУ УЧЕНЬ ГРУПИ П41 БУДНИЙ МАТВІЙ.")
     elif data == "btn_5.2":
-        await callback_query.message.answer("В процесі...")
+        await callback_query.message.answer("Повернення в головне меню...")
+        await show_main_menu(callback_query.message)
+
--- handlers/reply_handlers.py
+++ handlers/reply_handlers.py
@@ -1,28 +1,39 @@
 from aiogram import types, Router
-from keyboards.inline_keyboards import get_inline_keyboard, get_inline_keyboard2, get_inline_keyboard3, get_inline_keyboard4, get_inline_keyboard5
+from aiogram.types import FSInputFile
+
+from keyboards.inline_keyboards import get_inline_keyboard, get_inline_keyboard2, get_inline_keyboard3, \
+    get_inline_keyboard4, get_inline_keyboard5
 
 router = Router()
 
+
 @router.message(lambda message: message.text == "Готові пропозиції")
 async def test_handler(message: types.Message):
-    await message.answer("""В процесі""", reply_markup=get_inline_keyboard())
+    await message.answer("""Тут є всі готові пропозиції які є на даний момент.""", reply_markup=get_inline_keyboard())
+
 
 @router.message(lambda message: message.text == "Пропозиції по містам")
 async def test_handler(message: types.Message):
-    await message.answer("""В процесі""", reply_markup=get_inline_keyboard2())
+    await message.answer("""Введіть місто щоб знайти події в ньому:""", reply_markup=get_inline_keyboard2())
+
 
 @router.message(lambda message: message.text == "Бронювання")
 async def test_handler(message: types.Message):
-    await message.answer("""В процесі""", reply_markup=get_inline_keyboard3())
+    await message.answer("""Тут можна забронювати щось наперед. Напишіть те що ви забронювали:""", reply_markup=get_inline_keyboard3())
+
 
 @router.message(lambda message: message.text == "Акції")
 async def test_handler(message: types.Message):
-    await message.answer("""В процесі""", reply_markup=get_inline_keyboard4())
+    await message.answer("""Тут є всі акції на даний момент.""", reply_markup=get_inline_keyboard4())
+
 
 @router.message(lambda message: message.text == "Контакти")
 async def test_handler(message: types.Message):
-    await message.answer("""В процесі""", reply_markup=get_inline_keyboard5())
+    await message.answer("""Тут є контакти розробника.Нажми на кнопку щоб дізнатися їх.""",
+                         reply_markup=get_inline_keyboard5())
+
 
 @router.message(lambda message: message.text == "Пасхалочка")
 async def test_handler(message: types.Message):
-    await message.answer("""В процесі""")
+    photo = FSInputFile(r"C:\Users\asus\Documents\GitHub\TravelBot\assets\playboi-carti-gq-december-january-2021-02.jpg")  # Замініть шлях на реальний шлях до зображення
+    await message.answer_photo(photo, caption="Ось ваша пасхалочка! 🐣")
